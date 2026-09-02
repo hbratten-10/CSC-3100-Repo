4d43a3d925bbfbd855856f4310e49e933cb7a453
hbratten-10 <brattengirl10@gmail.com>
2026-09-02 09:50:25 -0700

    step 4 complete

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -20,7 +20,7 @@ const users = {
     },
     {
       id: "yat999",
-      name: "Dee",
+      name: "fred",
       job: "Aspring actress"
     },
     {
@@ -33,6 +33,23 @@ const users = {
 
 const app = express();
 const port = 8000;
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
 
 app.use(express.json());
 
