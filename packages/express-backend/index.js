a7ef0838d8abed336212b92dd06798d06fe9a568
hbratten-10 <brattengirl10@gmail.com>
2026-09-04 09:09:50 -0700

    Lab 2 Step 5

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -34,20 +34,16 @@ const users = {
 const app = express();
 const port = 8000;
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
 
